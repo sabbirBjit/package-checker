77acd0801a39b894633ce494d91988e92f533d06
sabbirBjit <ahmed.sabbir@bjitgroup.com>
2024-01-12 17:39:50 +0600

    added modal

--- src/pages/SVGViewer.tsx
+++ src/pages/SVGViewer.tsx
@@ -6,13 +6,31 @@ import {
   Snackbar,
   TextField,
   Grid,
+  Modal,
 } from "@mui/material";
 import { ImSvg } from "react-icons/im";
+import { IoIosCloseCircle } from "react-icons/io";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  borderRadius: 2,
+  p: 4,
+};
 
 const SVGViewer: React.FC = () => {
   const [files, setFiles] = useState<File[]>([]);
   const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false);
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
   const handleSnackbarClose = () => {
     setSnackbarOpen(false);
@@ -47,19 +65,59 @@ const SVGViewer: React.FC = () => {
           </Button>
         </Grid>
         <Grid item alignItems="stretch">
-            <TextField
-              label="Search"
-              variant="outlined"
-              disabled={!(files.length > 0)}
-              
-              sx={{ flexGrow: 1, minWidth: 0 }}
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-        
+          <TextField
+            label="Search"
+            variant="outlined"
+            disabled={!(files.length > 0)}
+            sx={{ flexGrow: 1, minWidth: 0 }}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
         </Grid>
       </Grid>
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <IoIosCloseCircle
+              size={24}
+              onClick={handleClose}
+              style={{ cursor: "pointer", color: "#1976d2" }}
+            />
 
+            {selectedFile && (
+              <img
+                src={URL.createObjectURL(selectedFile)}
+                alt={selectedFile?.name}
+                style={{ width: "100%" }}
+              />
+            )}
+
+            {selectedFile && (
+              <Button
+                onClick={() => {
+                  navigator.clipboard.writeText(selectedFile.name);
+                  setSnackbarOpen(true);
+                }}
+                sx={{
+                  marginTop: "8px",
+                  textAlign: "center",
+                  cursor: "pointer",
+                  borderRadius: "4px",
+                  padding: "8px",
+                  width: "100%",
+                }}
+              >
+                {selectedFile?.name}
+              </Button>
+            )}
+          </Box>
+        </Modal>
+      </div>
       <Box
         sx={{
           display: "grid",
@@ -85,8 +143,9 @@ const SVGViewer: React.FC = () => {
                 cursor: "pointer",
               }}
               onClick={() => {
-                navigator.clipboard.writeText(file.name);
-                setSnackbarOpen(true);
+                setSelectedFile(file);
+
+                handleOpen();
               }}
             >
               <div style={{ flex: 1, marginBottom: "8px" }}>
@@ -127,6 +186,7 @@ const SVGViewer: React.FC = () => {
         autoHideDuration={3000}
         onClose={handleSnackbarClose}
         message="File name copied to clipboard!"
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
       />
     </Box>
   );
